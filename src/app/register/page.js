6d8b03d1bd3076bd4549293ccda60ee9b38ac78c
Omar Ahmed <oa72357@gmail.com>
2025-07-04 17:54:39 +0300

    Second Commit

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,69 +1,10 @@
-"use client";
-
-import { useSearchParams, useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense } from "react";
+import RegisterForm from "./RegisterForm";
 
 export default function RegisterPage() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const role = searchParams.get("role");
-  const [isValid, setIsValid] = useState(true);
-
-  useEffect(() => {
-    if (role !== "student" && role !== "teacher") {
-      setIsValid(false);
-      router.push("/role-selection");
-    }
-  }, [role, router]);
-
   return (
-    <div
-      className="container d-flex justify-content-center align-items-center"
-      style={{ height: "90vh" }}
-    >
-      <div
-        className="card p-4 shadow"
-        style={{ maxWidth: "500px", width: "100%" }}
-      >
-        <h3 className="text-center mb-4">
-          Register as {role === "student" ? "Student" : "Teacher"}
-        </h3>
-        <form>
-          <div className="mb-3">
-            <label className="form-label" htmlFor="name">
-              Name
-            </label>
-            <input type="text" className="form-control" id="name" />
-          </div>
-
-          {role === "teacher" && (
-            <div className="mb-3">
-              <label className="form-label" htmlFor="Specialization">
-                Specialization
-              </label>
-              <input type="text" className="form-control" id="Specialization" />
-            </div>
-          )}
-
-          <div className="mb-3">
-            <label className="form-label" htmlFor="email">
-              Email
-            </label>
-            <input type="email" className="form-control" id="email" />
-          </div>
-
-          <div className="mb-3">
-            <label className="form-label" htmlFor="Password">
-              Password
-            </label>
-            <input type="password" className="form-control" id="Password" />
-          </div>
-
-          <button type="submit" className="btn btn-dark w-100">
-            Register
-          </button>
-        </form>
-      </div>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <RegisterForm />
+    </Suspense>
   );
 }
